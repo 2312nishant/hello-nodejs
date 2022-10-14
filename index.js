bf4cfa68068a02aacfcc09bdeafb8064d216201b
Nishant S Nair <2312nishant@users.noreply.github.com>
2022-10-14 09:39:01 +0530

    Updated with route

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const requestUrl = url.parse(req.url);
+  const path = requestUrl.pathname;
+
+  const parts = path.split('/').slice(1);
+
+  // This is really brittle, but assuming you know it's going to be 2 parts remaining after the above..
+
   res.statusCode = 200;
-  const msg = 'Hello Node : East US!\n'
-  res.end(msg);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(parts[1]);
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
